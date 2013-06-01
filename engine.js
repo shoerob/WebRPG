59236bf55559b843c859f6a96fe1de66338b91a8
Robert Shoemate <shoerob@gmail.com>
2013-06-01 15:24:29 -0500

    first commit

---
+++ engine.js
@@ -0,0 +1,109 @@
+var canvas;
+var ctx;
+
+/// map
+var map = [];
+var players = [];
+
+/// Player
+var Player = function(data) {
+	this.id = data.id;
+	this.xPos = data.x;
+	this.yPos = data.y;
+};
+Player.prototype.update = function(data) {
+	this.xPos = data.x;
+	this.yPos = data.y;
+}
+
+// character
+var socket;
+var charX = 1;
+var charY = 1;
+
+var Engine = function(canvas) {
+	canvas = canvas;
+	ctx = canvas.getContext('2d');
+
+}
+Engine.prototype.start = function() {
+
+	// connect to socket.io
+	socket = io.connect('http://localhost:8081');
+
+	// update movement of another player
+	socket.on('updatePlayer', function(data) {
+		for (var i = 0; i < players.length; i++) {
+			if (players[i].id == data.id) {
+				players[i].update(data);
+			}
+		}
+	});
+
+	// another player connected
+	socket.on('playerConnected', function(data) {
+		console.log('Player connected.');
+		players.push(new Player(data));
+	});
+
+	// another player disconnected
+	socket.on('playerDisconnected', function(data) {
+		console.log('Player disconnected.');
+		for (var i = 0; i < players.length; i++) {
+			if (players[i].id == data.id) {
+				players.splice(i, 1);
+			}
+		}
+	});
+
+	// a new map was loaded
+	socket.on('updateMap', function(data) {
+		map = data.map;
+		console.log(map);
+	});
+
+	// start the update/render loop
+	setInterval(this.render, 60);
+}
+
+Engine.prototype.render = function() {
+	ctx.clearRect(0,0,640,480);
+
+	// render map
+	for (var i = 0; i < map.length; i++) {
+		for (var j = 0; j < map[i].length; j++) {
+			if (map[i][j] == 0)
+				ctx.fillStyle = "rgb(200,200,200)";
+				
+			if (map[i][j] == 1)
+				ctx.fillStyle = "rgb(200,0,0)";
+
+			if (map[i][j] == 2)
+				ctx.fillStyle = "rgb(200,200,0)";
+
+			ctx.fillRect (j * 51, i * 51, 50, 50);
+		}
+	}
+
+	// render character
+	ctx.fillStyle = "rgb(0,200,0)";
+	ctx.fillRect (charX * 51, charY * 51, 50, 50);
+
+	// render peers
+	for (var i = 0; i < players.length; i++) {
+		var player = players[i];
+		ctx.fillStyle = "rgb(0,200,0)";
+		ctx.fillRect (player.xPos * 51, player.yPos * 51, 50, 50);
+	}
+}
+
+function onKeyDown(event) {
+	socket.emit('keyDown', { keyCode : event.keyCode }, function(position) {
+		charX = position.x;
+		charY = position.y;
+	});
+}
+
+function onKeyUp(event) {
+	
+}
---
+++ node/server.js
@@ -0,0 +1,95 @@
+var io = require('socket.io').listen(8081);
+var fs = require('fs');
+
+// /// map
+ var map = [];
+
+fs.readFile( __dirname + '/maps/map1.json', 'utf8', function(err, data) {
+		if (err) {
+			throw err;
+		}
+		map = JSON.parse(data);
+		console.log(map);
+	})
+
+var players = [];
+
+/// client
+var Player = function (socket) {
+	this.socket = socket;
+	this.createDisconnectCallback(this);
+	this.createKeyDownCallback(this);
+
+	this.xPos = 1;
+	this.yPos = 1;
+};
+Player.prototype.createDisconnectCallback = function(player) {
+	player.socket.on('disconnect', function() {
+		console.log('Client disconnected.');
+
+		for (var i = 0; i < players.length; i++) {
+			if (players[i].socket.id == player.socket.id) {
+				players.splice(i, 1);
+			}
+		}
+
+		player.socket.broadcast.emit('playerDisconnected', { id : player.socket.id });
+	});
+}
+Player.prototype.createKeyDownCallback = function(player) {
+	// movement
+	player.socket.on('keyDown', function(data, fn) {
+
+		switch(data.keyCode) {
+			case 37: // left
+				if (player.xPos > 0 && map[player.yPos][player.xPos - 1] != 0)
+					player.xPos--;
+			break;
+			
+			case 38: // up
+				if (player.yPos > 0 && map[player.yPos - 1][player.xPos] != 0)
+					player.yPos--;
+			break;
+			
+			case 39: // right
+				if (player.xPos < map[player.yPos].length - 1 && map[player.yPos][player.xPos + 1] != 0)
+					player.xPos++;
+			break;
+			
+			case 40: // down
+				if (player.yPos < map.length - 1 && map[player.yPos + 1][player.xPos] != 0)
+					player.yPos++;
+			break;
+				
+		}
+
+		// update the client on the new player position
+		fn({ x : player.xPos, y : player.yPos });
+
+		// update other clients on this players position
+		player.socket.broadcast.emit('updatePlayer', { id : player.socket.id, x : player.xPos, y : player.yPos });
+	})
+}
+Player.prototype.notifyPeersConnected = function() {
+	this.socket.broadcast.emit('playerConnected', { id : this.socket.id, x : this.xPos, y : this.yPos });
+}
+
+/// sockets
+io.sockets.on('connection', function(socket) {
+	console.log('Client connected.');
+
+	socket.emit('updateMap', { map : map });
+
+	// inform the player that connected of all connected peers
+	for (var i = 0; i < players.length; i++) {
+		var peer = players[i];
+		socket.emit('playerConnected', { id : peer.socket.id, x : peer.xPos, y : peer.yPos })
+	}
+
+	// add the player to the connected peers
+	var player = new Player(socket);
+	players.push(player);
+
+	// notify peers that this player connected
+	player.notifyPeersConnected();
+});
